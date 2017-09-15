f395b96e0e6693fe6c184733a3ce0f40e58f1c33
Adham <adhamwynston@outlook.com>
2017-09-15 12:50:33 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+  <navbar></navbar>
+  </div>
+</template>
+
+<script>
+  import Ccnav from './components/Navbar.vue'
+export default {
+  components: {
+      'navbar': Ccnav
+  }
+}
+</script>
+
+<style>
+
+</style>
